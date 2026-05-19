c43cbfc189d44625b976a4fa237565203f324382
klyroe <keylaanmrr@gmail.com>
2026-05-19 12:12:28 +0700

    add radio button component

---
+++ app/src/main/java/com/desacibiruwetan/posyandu/ui/components/input/AppRadioButton.kt
@@ -0,0 +1,48 @@
+package com.desacibiruwetan.posyandu.ui.components.input
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.RadioButtonChecked
+import androidx.compose.material.icons.filled.RadioButtonUnchecked
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.desacibiruwetan.posyandu.ui.theme.Inter
+import com.desacibiruwetan.posyandu.ui.theme.PrimaryGreen
+
+@Composable
+fun AppRadioButton(
+    text: String,
+    isSelected: Boolean,
+    onClick: () -> Unit
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.clickable { onClick() }
+    ) {
+        Icon(
+            imageVector = if (isSelected) Icons.Default.RadioButtonChecked else Icons.Default.RadioButtonUnchecked,
+            contentDescription = null,
+            tint = if (isSelected) PrimaryGreen else Color(0xFFC9C9C9),
+            modifier = Modifier.size(20.dp)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Text(
+            text = text,
+            fontFamily = Inter,
+            fontWeight = FontWeight.Normal,
+            fontSize = 12.sp,
+            color = Color(0xFF272727)
+        )
+    }
+}
