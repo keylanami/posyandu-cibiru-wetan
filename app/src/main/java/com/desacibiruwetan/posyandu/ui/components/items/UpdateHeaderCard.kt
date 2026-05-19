e6fd16a08e955fdd5135423ad84e4c95a6579a4f
klyroe <keylaanmrr@gmail.com>
2026-05-19 12:11:53 +0700

    add header card for update screens

---
+++ app/src/main/java/com/desacibiruwetan/posyandu/ui/components/items/UpdateHeaderCard.kt
@@ -0,0 +1,58 @@
+package com.desacibiruwetan.posyandu.ui.components.items
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.desacibiruwetan.posyandu.ui.screen.warga.CircularIconBox
+import com.desacibiruwetan.posyandu.ui.theme.Inter
+import com.desacibiruwetan.posyandu.ui.theme.SurfaceWhite
+
+@Composable
+fun UpdateHeaderCard(
+    title: String = "Update untuk", name: String, icon: ImageVector
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .shadow(16.dp, spotColor = Color(0x40DFDFDF), shape = RoundedCornerShape(15.dp))
+            .background(SurfaceWhite, RoundedCornerShape(15.dp))
+            .padding(24.dp)
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            CircularIconBox(icon = icon)
+            Spacer(modifier = Modifier.width(16.dp))
+            Column {
+                Text(
+                    text = title,
+                    fontFamily = Inter,
+                    fontWeight = FontWeight.SemiBold,
+                    fontSize = 12.sp,
+                    color = Color(0xFF8B8B8B)
+                )
+                Text(
+                    text = name,
+                    fontFamily = Inter,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 15.sp,
+                    color = Color(0xFF272727)
+                )
+            }
+        }
+    }
+}
