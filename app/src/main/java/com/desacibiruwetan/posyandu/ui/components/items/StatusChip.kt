21c3525863407187ad9b237db9e6ebdabb494195
klyroe <keylaanmrr@gmail.com>
2026-05-19 10:10:42 +0700

    add status chip

---
+++ app/src/main/java/com/desacibiruwetan/posyandu/ui/components/items/StatusChip.kt
@@ -0,0 +1,57 @@
+package com.desacibiruwetan.posyandu.ui.components.items
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.desacibiruwetan.posyandu.ui.theme.Inter
+
+@Composable
+fun StatusChip(
+    text: String,
+    icon: ImageVector,
+    containerColor: Color,
+    borderColor: Color,
+    contentColor: Color
+) {
+    Row(
+        modifier = Modifier
+            .height(43.dp)
+            .background(color = containerColor, shape = RoundedCornerShape(12.dp))
+            .border(width = 1.dp, color = borderColor, shape = RoundedCornerShape(12.dp))
+            .padding(horizontal = 12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = text,
+            tint = contentColor,
+            modifier = Modifier.size(20.dp)
+        )
+        Spacer(modifier = Modifier.width(6.dp))
+        Text(
+            text = text,
+            fontFamily = Inter,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 10.sp,
+            color = contentColor
+        )
+    }
+}
