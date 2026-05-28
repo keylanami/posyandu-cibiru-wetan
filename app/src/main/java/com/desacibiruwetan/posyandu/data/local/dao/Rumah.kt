548d6014bc912f14d4b357a94535fd1519096df7
klyroe <keylaanmrr@gmail.com>
2026-05-29 03:52:45 +0700

    add rumah dao

---
+++ app/src/main/java/com/desacibiruwetan/posyandu/data/local/dao/Rumah.kt
@@ -0,0 +1,29 @@
+package com.desacibiruwetan.posyandu.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.desacibiruwetan.posyandu.data.local.entity.RumahEntity
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface RumahDao{
+
+    @Query("select * from tabel_rumah order by localId desc")
+    fun getAllRumahDao(): Flow<List<RumahEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRumahLocal(rumahEntity: RumahEntity): Long
+
+    @Update
+    suspend fun updateRumahLocal(rumah: RumahEntity)
+
+    @Query("select * from tabel_rumah where isSynced = 0")
+    suspend fun getRumahBelumSinkron(): List<RumahEntity>
+
+    @Query("delete from tabel_rumah")
+    suspend fun deleteAllRumahLocal()
+}
