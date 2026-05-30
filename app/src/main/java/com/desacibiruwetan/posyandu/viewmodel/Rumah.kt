eb65677552422fc62d2ae1236703e703b511d0c2
klyroe <keylaanmrr@gmail.com>
2026-05-31 01:05:06 +0700

    add rumah vm

---
+++ app/src/main/java/com/desacibiruwetan/posyandu/viewmodel/Rumah.kt
@@ -0,0 +1,38 @@
+package com.desacibiruwetan.posyandu.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.desacibiruwetan.posyandu.data.local.entity.RumahEntity
+import com.desacibiruwetan.posyandu.data.repository.RumahRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class RumahViewmodel(private val repository: RumahRepository): ViewModel(){
+
+    val listRumahLocal: StateFlow<List<RumahEntity>> = repository.getAllRumahLocal().stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue =  emptyList()
+    )
+
+    fun syncDataRumah(token: String){
+        viewModelScope.launch {
+            repository.pullDataFromServer(token)
+        }
+    }
+
+
+    fun tambahRumah(token: String, alamat: String, noRumah: String){
+        viewModelScope.launch {
+            repository.addNewRumah(token, alamat, noRumah)
+        }
+    }
+
+    fun updateRumah(token: String, rumahLocal: RumahEntity, alamatBaru: String, noRumahBaru: String){
+        viewModelScope.launch {
+            repository.updateRumah(token, rumahLocal, alamatBaru, noRumahBaru)
+        }
+    }
+}
