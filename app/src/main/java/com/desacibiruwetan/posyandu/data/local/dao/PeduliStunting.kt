9a0beee99e7921d08c8b8de061848cb40331ebdf
klyroe <keylaanmrr@gmail.com>
2026-06-21 01:59:49 +0700

    add peduli stunting dao

--- app/src/main/java/com/desacibiruwetan/posyandu/data/local/dao/PeduliStunting.kt
+++ app/src/main/java/com/desacibiruwetan/posyandu/data/local/dao/PeduliStunting.kt
@@ -1,2 +1,28 @@
 package com.desacibiruwetan.posyandu.data.local.dao
 
+import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Update
+import com.desacibiruwetan.posyandu.data.local.entity.PeduliStuntingEntity
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface PeduliStuntingDao {
+
+    @Update
+    suspend fun updatePeduliStuntingLocal(peduliStunting: PeduliStuntingEntity)
+
+    @Query("select * from tabel_peduli_stunting order by idPeduliStuntingLocal desc")
+    fun getAllPeduliStuntingDao(): Flow<List<PeduliStuntingEntity>>
+
+    @Query("select * from tabel_peduli_stunting where isSynced = 0")
+    suspend fun getPeduliStuntingBelumSinkron(): List<PeduliStuntingEntity>
+
+    @Query("delete from tabel_peduli_stunting")
+    suspend fun deleteAllPeduliStuntingLocal()
+
+    @Query("select * from tabel_peduli_stunting where idPeduliStuntingLocal = :localId or idPeduliStuntingServer = :serverId limit 1")
+    suspend fun getPeduliStuntingByAnggotaId(localId: Int, serverId: Int?): PeduliStuntingEntity?
+
+}
