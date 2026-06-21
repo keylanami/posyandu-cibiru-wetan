dde5f972b5ee835028002e48c3c77b798f32e29c
klyroe <keylaanmrr@gmail.com>
2026-06-21 10:01:58 +0700

    add kia dao

--- app/src/main/java/com/desacibiruwetan/posyandu/data/local/dao/Kia.kt
+++ app/src/main/java/com/desacibiruwetan/posyandu/data/local/dao/Kia.kt
@@ -2,6 +2,7 @@ package com.desacibiruwetan.posyandu.data.local.dao
 
 import androidx.room.Dao
 import androidx.room.Query
+import androidx.room.Update
 import com.desacibiruwetan.posyandu.data.local.entity.KiaEntity
 import kotlinx.coroutines.flow.Flow
 
@@ -10,6 +11,18 @@ import kotlinx.coroutines.flow.Flow
 interface KiaDao {
     @Query("select * from tabel_kia order by id desc")
     fun getAllKiaDao(): Flow<List<KiaEntity>>
+
+    @Query("select * from tabel_kia where isSynced = 0")
+    fun getKiaBelumSinkron(): List<KiaEntity>
+
+    @Query("delete from tabel_kia")
+    fun deleteAllKiaLocal()
+
+    @Query("select * from tabel_kia where id = :localId or idServer = :serverId limit 1")
+    fun getKiaByAnggotaId(localId: Int, serverId: Int): KiaEntity?
+
+    @Update
+    fun updateKiaLocal(kia: KiaEntity)
 
 
 }
